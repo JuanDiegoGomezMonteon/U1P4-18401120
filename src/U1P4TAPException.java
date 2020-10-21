c5b7f681bf65a909317dadb2bc9b431af74d668b
JuanDiegoGomezMonteon <judigomezmo@ittepic.edu.mx>
2020-10-21 17:41:20 -0600

    version 1.0

---
+++ src/U1P4TAPException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class U1P4TAPException extends RuntimeException {
+    public U1P4TAPException (String message){
+        super(message);
+    }
+}
